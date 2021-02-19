898693a6d2866fdaf1ca7a4f860f80c9886e938a
Parth Makadiya <makadiyaparth98@gmail.com>
2021-02-19 07:41:41 +0530

    initial commit

---
+++ src/main/java/com/pm/springbootmysqltemplate/SpringBootMysqlTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.pm.springbootmysqltemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMysqlTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMysqlTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pm/springbootmysqltemplate/SpringBootMysqlTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.springbootmysqltemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMysqlTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
